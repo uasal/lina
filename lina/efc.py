8c785261d9bf47f650504f90b6cb96c71ce86f26
Kian Milani <kianmilani@email.arizona.edu>
2024-10-02 16:04:49 -0700

    making sure this is up to date

--- lina/efc.py
+++ lina/efc.py
@@ -8,31 +8,31 @@ import time
 import copy
 from IPython.display import display, clear_output
 
-def compute_jacobian(M,
-                     calib_modes,
-                     control_mask,
-                     amp=1e-9,
-                     plot_responses=True,
-                     ):
-    Nmodes = calib_modes.shape[0]
-    Nmask = int(control_mask.sum())
-    jac = xp.zeros((2*Nmask, Nmodes))
+# def compute_jacobian(M,
+#                      calib_modes,
+#                      control_mask,
+#                      amp=1e-9,
+#                      plot_responses=True,
+#                      ):
+#     Nmodes = calib_modes.shape[0]
+#     Nmask = int(control_mask.sum())
+#     jac = xp.zeros((2*Nmask, Nmodes))
 
-    for i in range(Nmodes):
-        E_pos = M.forward(amp*calib_modes[i][M.dm_mask], use_wfe=True, use_vortex=True)[control_mask]
-        E_neg = M.forward(-amp*calib_modes[i][M.dm_mask], use_wfe=True, use_vortex=True)[control_mask]
-        response = (E_pos - E_neg)/(2*amp)
-        jac[::2,i] = xp.real(response)
-        jac[1::2,i] = xp.imag(response)
+#     for i in range(Nmodes):
+#         E_pos = M.forward(amp*calib_modes[i][M.dm_mask], use_wfe=True, use_vortex=True)[control_mask]
+#         E_neg = M.forward(-amp*calib_modes[i][M.dm_mask], use_wfe=True, use_vortex=True)[control_mask]
+#         response = (E_pos - E_neg)/(2*amp)
+#         jac[::2,i] = xp.real(response)
+#         jac[1::2,i] = xp.imag(response)
 
-    if plot_responses:
-        dm_rms = xp.sqrt(xp.mean(xp.square(jac.dot(calib_modes)), axis=0))
-        dm_rms = dm_rms.reshape(M.Nact, M.Nact) / xp.max(dm_rms)
-        imshows.imshow1(dm_rms, 'DM RMS Actuator Responses', lognorm=True, vmin=1e-2)
+#     if plot_responses:
+#         dm_rms = xp.sqrt(xp.mean(xp.square(jac.dot(calib_modes)), axis=0))
+#         dm_rms = dm_rms.reshape(M.Nact, M.Nact) / xp.max(dm_rms)
+#         imshows.imshow1(dm_rms, 'DM RMS Actuator Responses', lognorm=True, vmin=1e-2)
 
-    return jac
+#     return jac
     
-def calibrate(sysi, 
+def calibrate(I, 
               calibration_modes, calibration_amp,
               control_mask, 
               scc_fun=None, scc_params=None,
@@ -63,18 +63,18 @@ def calibrate(sysi,
     Nmask = int(control_mask.sum())
     
     response_matrix = xp.zeros((2*Nmask, Nmodes), dtype=xp.float64)
-    if return_all: response_cube = xp.zeros((sysi.npsf**2, 2, Nmodes))
+    if return_all: response_cube = xp.zeros((I.npsf**2, 2, Nmodes))
     print('Calculating Jacobian: ')
     for i,dm_mode in enumerate(calibration_modes):
         response = 0
         for s in [-1, 1]:
             # Add the mode to the DMs
-            sysi.add_dm(s * calibration_amp * dm_mode.reshape(sysi.Nact,sysi.Nact))
+            I.add_dm(s * calibration_amp * dm_mode.reshape(I.Nact,I.Nact))
             # Compute/Measure reponse with model or SCC
-            efield = sysi.calc_wf() if scc_fun is None else scc_fun(sysi, **scc_params)
+            efield = I.calc_wf() if scc_fun is None else scc_fun(I, **scc_params)
             response += s * efield / (2 * calibration_amp)
             # Remove the mode form the DMs
-            sysi.add_dm(-s * calibration_amp * dm_mode.reshape(sysi.Nact,sysi.Nact))
+            I.add_dm(-s * calibration_amp * dm_mode.reshape(I.Nact,I.Nact))
 
         response_matrix[::2,i] = response[control_mask].real
         response_matrix[1::2,i] = response[control_mask].imag
@@ -91,7 +91,7 @@ def calibrate(sysi,
     if plot:
         dm_responses = response_matrix[::2] + 1j*response_matrix[1::2]
         dm_responses = dm_responses.dot(xp.array(calibration_modes.reshape(Nmodes, -1)))
-        dm_response_map = xp.sqrt(xp.mean(xp.abs(dm_responses)**2, axis=0)).reshape(sysi.Nact, sysi.Nact)
+        dm_response_map = xp.sqrt(xp.mean(xp.abs(dm_responses)**2, axis=0)).reshape(I.Nact, I.Nact)
         dm_response_map /= xp.max(dm_response_map)
         imshows.imshow1(dm_response_map, lognorm=True, vmin=1e-2)
 
@@ -100,7 +100,7 @@ def calibrate(sysi,
     else:
         return response_matrix
 
-def run(sysi, 
+def run(I, 
         control_matrix,
         calibration_modes,
         control_mask, 
@@ -121,12 +121,13 @@ def run(sysi,
     start = time.time()
     calibration_modes = xp.array(calibration_modes)
     Nmodes = calibration_modes.shape[0]
+    modal_matrix = calibration_modes.reshape(Nmodes, -1).T
     Nmask = int(control_mask.sum())
     starting_itr = len(all_ims)
     if len(all_commands)>0:
         total_command = copy.copy(all_commands[-1])
     else:
-        total_command = xp.zeros((sysi.Nact,sysi.Nact))
+        total_command = xp.zeros((I.Nact,I.Nact))
     efield_ri = xp.zeros(2*Nmask)
     for i in range(iterations):
         print(f"\tClosed-loop iteration {i+1+starting_itr} / {iterations+starting_itr}")
@@ -136,18 +137,18 @@ def run(sysi,
             electric_field = est_fun(sysi, **est_params)
         else:
             print('Using model to compute electric field')
-            electric_field = sysi.calc_wf() # no PWP, just use model
+            electric_field = I.calc_wf() # no PWP, just use model
         
         efield_ri[::2] = electric_field[control_mask].real
         efield_ri[1::2] = electric_field[control_mask].imag
 
         modal_coeff = -control_matrix.dot(efield_ri)
-        # del_command = calibration_modes.T.dot(modal_coeff).reshape(sysi.Nact,sysi.Nact)
-        del_command = calibration_modes.reshape(Nmodes, -1).T.dot(modal_coeff).reshape(sysi.Nact,sysi.Nact)
+        del_command = modal_matrix.dot(modal_coeff).reshape(I.Nact,I.Nact)
+        # del_command = calibration_modes.reshape(Nmodes, -1).T.dot(modal_coeff).reshape(I.Nact,I.Nact)
         total_command = (1.0-leakage)*total_command + loop_gain*del_command
-        sysi.set_dm(total_command)
+        I.set_dm(total_command)
 
-        image = sysi.snap()
+        image = I.snap()
 
         all_ims.append(copy.copy(image))
         all_efs.append(copy.copy(electric_field))
@@ -161,13 +162,13 @@ def run(sysi,
                     'Total DM Command', 
                     f'Image\nMean NI = {mean_ni:.3e}',
                     cmap1='viridis', cmap2='viridis', 
-                    pxscl3=sysi.psf_pixelscale_lamD, lognorm3=True, vmin3=1e-9)
+                    pxscl3=I.psf_pixelscale_lamD, lognorm3=True, vmin3=1e-9)
             
             # if plot_sms:
             #     sms_fig = sms(U, s, alpha2, efield_ri, Nmask, i)
 
             if plot_radial_contrast:
-                utils.plot_radial_contrast(image, control_mask, sysi.psf_pixelscale_lamD, nbins=100)
+                utils.plot_radial_contrast(image, control_mask, I.psf_pixelscale_lamD, nbins=100)
             
             if not plot_all: clear_output(wait=True)
                 
