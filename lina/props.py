82b3567675cf0beae054275f8edb4d28ece8214c
Kian <kianmilani@arizona.edu>
2026-02-02 09:58:02 -0700

    adding docstrings to props.py

--- lina/props.py
+++ lina/props.py
@@ -10,14 +10,54 @@ from pathlib import Path
 import copy
 
 def fft(arr):
+    """
+    Compute the 2D Fourier Transform of an array with proper FFT shifts applied for the DC component. 
+
+    Args:
+        arr (ndarray): 
+            2D array to be Fourier Transformed.
+
+    Returns:
+        ndarray:
+            The Fourier Transform of the input array.  
+    """
     return xp.fft.ifftshift(xp.fft.fft2(xp.fft.fftshift(arr)))
     # return xp.fft.ifftshift(xp.fft.fft2(arr))
 
 def ifft(arr):
+    """
+    Compute the 2D Fourier Transform of an array with proper FFT shifts applied for the DC component. 
+
+    Args:
+        arr (ndarray): 
+            2D array to be Fourier Transformed.
+
+    Returns:
+        ndarray:
+            The Fourier Transform of the input array.  
+    """
     return xp.fft.fftshift(xp.fft.ifft2(xp.fft.ifftshift(arr)))
     # return xp.fft.fftshift(xp.fft.ifft2(arr))
 
 def ang_spec(wavefront, wavelength, distance, pixelscale):
+    """
+    Prpoagates a wavefront a given distance using the angular spectrum propagation method. 
+    The resulting wavefront will have the same pixelscale as the input wavefront. 
+
+    Args:
+        wavefront (ndarray): 
+            A 2D array representing the wavefront to be propagated. 
+        wavelength (float): 
+            The wavelength of the wavefront in units of meters. 
+        distance (float): 
+            The distance to propagate in units of meters. 
+        pixelscale (float): 
+            The pixelscale of the current wavefront in units of meters/pixel. 
+
+    Returns:
+        ndarray: 
+            The 2D array for the propagated wavefront. 
+    """
     n = wavefront.shape[0]
 
     delkx = 2*np.pi/(n*pixelscale)
@@ -45,6 +85,35 @@ def make_mft_forward_matrices(
         pp_centering='odd', 
         fp_centering='odd',
     ):
+    """
+    Generate the matrices required to perform a forward Matrix Fourier Transform. 
+
+    Args:
+        N (int): 
+            The number of pixels across the array that will be Fourier Transformed
+        npix (int): 
+            The number of pixels across pupil/beam in the array 
+            that will be Fourier Transformed. 
+        npsf (int): 
+            The number of pixels across the focal plane the Fourier Transform will compute. 
+        psf_pixelscale_lamD (float): 
+            The desired pixelscale of the focal plane the Fourier Transform will compute
+            in units of lambda/D. 
+        convention (str, optional): 
+            The sign convention to use for the Fourier Transform. Defaults to '-'. 
+        pp_centering (str, optional): 
+            The centering of the input wavefront. Odd means centered on a single pixel,
+            even means ceneterd at the quadrant of four pixels. Defaults to 'odd'.
+        fp_centering (str, optional): 
+            The desired centering of the focal plane wavefront. Odd means centered on a single pixel,
+            even means ceneterd at the quadrant of four pixels. Defaults to 'odd'.
+
+    Returns:
+        tuple:
+            Returns two ndarrays and a float for the normalization coefficient for the MFT. 
+    """
+
+
     dx = 1.0 / npix
     if pp_centering=='even':
         Xs = (xp.arange(N, dtype=float) - (N / 2) + 1/2) * dx
@@ -80,30 +149,68 @@ def mft_forward(
         pp_centering='odd', 
         fp_centering='odd',
     ):
+    """
+    Generate the matrices required to perform a forward Matrix Fourier Transform. 
+
+    Args:
+        wavefront (ndarray): 
+            The input wavefront that will be Fourier Transformed
+        npix (int): 
+            The number of pixels across pupil/beam in the array 
+            that will be Fourier Transformed. 
+        npsf (int): 
+            The number of pixels across the focal plane the Fourier Transform will compute. 
+        psf_pixelscale_lamD (float): 
+            The desired pixelscale of the focal plane the Fourier Transform will compute
+            in units of lambda/D. 
+        convention (str, optional): 
+            The sign convention to use for the Fourier Transform. Defaults to '-'. 
+        pp_centering (str, optional): 
+            The centering of the input wavefront. Odd means centered on a single pixel,
+            even means ceneterd at the quadrant of four pixels. Defaults to 'odd'.
+        fp_centering (str, optional): 
+            The desired centering of the focal plane wavefront. Odd means centered on a single pixel,
+            even means ceneterd at the quadrant of four pixels. Defaults to 'odd'.
+
+    Returns:
+        ndarray:
+            The Fourier Transform of the input wavefront. 
+            
+    """
     N = wavefront.shape[0]
-    dx = 1.0 / npix
-    if pp_centering=='even':
-        Xs = (xp.arange(N, dtype=float) - (N / 2) + 1/2) * dx
-    elif pp_centering=='odd':
-        Xs = (xp.arange(N, dtype=float) - (N / 2)) * dx
+    # dx = 1.0 / npix
+    # if pp_centering=='even':
+    #     Xs = (xp.arange(N, dtype=float) - (N / 2) + 1/2) * dx
+    # elif pp_centering=='odd':
+    #     Xs = (xp.arange(N, dtype=float) - (N / 2)) * dx
 
-    du = psf_pixelscale_lamD
-    if fp_centering=='odd':
-        Us = (xp.arange(npsf, dtype=float) - npsf / 2) * du
-    elif fp_centering=='even':
-        Us = (xp.arange(npsf, dtype=float) - npsf / 2 + 1/2) * du
+    # du = psf_pixelscale_lamD
+    # if fp_centering=='odd':
+    #     Us = (xp.arange(npsf, dtype=float) - npsf / 2) * du
+    # elif fp_centering=='even':
+    #     Us = (xp.arange(npsf, dtype=float) - npsf / 2 + 1/2) * du
 
-    xu = xp.outer(Us, Xs)
-    vy = xp.outer(Xs, Us)
+    # xu = xp.outer(Us, Xs)
+    # vy = xp.outer(Xs, Us)
 
-    if convention=='-':
-        My = xp.exp(-1j*2*np.pi*vy) 
-        Mx = xp.exp(-1j*2*np.pi*xu)
-    else:
-        My = xp.exp(1j*2*np.pi*vy) 
-        Mx = xp.exp(1j*2*np.pi*xu)
+    # if convention=='-':
+    #     My = xp.exp(-1j*2*np.pi*vy) 
+    #     Mx = xp.exp(-1j*2*np.pi*xu)
+    # else:
+    #     My = xp.exp(1j*2*np.pi*vy) 
+    #     Mx = xp.exp(1j*2*np.pi*xu)
 
-    norm_coeff = psf_pixelscale_lamD/npix
+    # norm_coeff = psf_pixelscale_lamD/npix
+
+    Mx, My, norm_coeff = make_mft_forward_matrices(
+        N, 
+        npix, 
+        npsf, 
+        psf_pixelscale_lamD, 
+        convention=convention, 
+        pp_centering=pp_centering, 
+        fp_centering=fp_centering,
+    )
 
     return Mx@wavefront@My * norm_coeff
 
@@ -116,6 +223,33 @@ def make_mft_reverse_matrices(
         pp_centering='odd', 
         fp_centering='odd',
     ):
+    """
+    Generate the matrices required to perform a reverse Matrix Fourier Transform. 
+
+    Args:
+        npsf (int): 
+            The number of pixels across the focal plane array that will be Fourier Transformed
+        psf_pixelscale_lamD (float): 
+            The pixelscale of the focal plane array that will be the Fourier Transformed
+            in units of lambda/D. 
+        npix (int): 
+            The desired number of pixels across pupil plane wavefront to be computed. 
+        N (int): 
+            The total number of pixels across the pupil plane wavefront to be computed. 
+        convention (str, optional): 
+            The sign convention to use for the Fourier Transform. Defaults to '+'. 
+        pp_centering (str, optional): 
+            The desired centering of the pupil plane wavefront. Odd means centered on a single pixel,
+            even means ceneterd at the quadrant of four pixels. Defaults to 'odd'.
+        fp_centering (str, optional): 
+            The centering of the focal plane wavefront. Odd means centered on a single pixel,
+            even means ceneterd at the quadrant of four pixels. Defaults to 'odd'.
+
+    Returns:
+        tuple:
+            Returns two ndarrays and a float for the normalization coefficient for the MFT. 
+    """
+
     du = psf_pixelscale_lamD
     if fp_centering=='odd':
         Us = (xp.arange(npsf, dtype=float) - npsf / 2) * du
@@ -151,30 +285,66 @@ def mft_reverse(
         pp_centering='odd', 
         fp_centering='odd',
     ):
+    """
+    Generate the matrices required to perform a reverse Matrix Fourier Transform. 
+
+    Args:
+        fpwf (ndarray): 
+            The focal plane wavefront to compute the Fourier Transform of. 
+        psf_pixelscale_lamD (float): 
+            The pixelscale of the focal plane array that will be the Fourier Transformed
+            in units of lambda/D. 
+        npix (int): 
+            The desired number of pixels across pupil plane wavefront to be computed. 
+        N (int): 
+            The total number of pixels across the pupil plane wavefront to be computed. 
+        convention (str, optional): 
+            The sign convention to use for the Fourier Transform. Defaults to '+'. 
+        pp_centering (str, optional): 
+            The desired centering of the pupil plane wavefront. Odd means centered on a single pixel,
+            even means ceneterd at the quadrant of four pixels. Defaults to 'odd'.
+        fp_centering (str, optional): 
+            The centering of the focal plane wavefront. Odd means centered on a single pixel,
+            even means ceneterd at the quadrant of four pixels. Defaults to 'odd'.
+
+    Returns:
+        ndarray:
+            The Fourier Transform of the input wavefront. 
+    """
     npsf = fpwf.shape[0]
-    du = psf_pixelscale_lamD
-    if fp_centering=='odd':
-        Us = (xp.arange(npsf, dtype=float) - npsf / 2) * du
-    elif fp_centering=='even':
-        Us = (xp.arange(npsf, dtype=float) - npsf / 2 + 1/2) * du
+    # du = psf_pixelscale_lamD
+    # if fp_centering=='odd':
+    #     Us = (xp.arange(npsf, dtype=float) - npsf / 2) * du
+    # elif fp_centering=='even':
+    #     Us = (xp.arange(npsf, dtype=float) - npsf / 2 + 1/2) * du
 
-    dx = 1.0 / npix
-    if pp_centering=='even':
-        Xs = (xp.arange(N, dtype=float) - (N / 2) + 1/2) * dx
-    elif pp_centering=='odd':
-        Xs = (xp.arange(N, dtype=float) - (N / 2)) * dx
+    # dx = 1.0 / npix
+    # if pp_centering=='even':
+    #     Xs = (xp.arange(N, dtype=float) - (N / 2) + 1/2) * dx
+    # elif pp_centering=='odd':
+    #     Xs = (xp.arange(N, dtype=float) - (N / 2)) * dx
 
-    ux = xp.outer(Xs, Us)
-    yv = xp.outer(Us, Xs)
+    # ux = xp.outer(Xs, Us)
+    # yv = xp.outer(Us, Xs)
 
-    if convention=='+':
-        My = xp.exp(1j*2*np.pi*yv) 
-        Mx = xp.exp(1j*2*np.pi*ux)
-    else:
-        My = xp.exp(-1j*2*np.pi*yv)
-        Mx = xp.exp(-1j*2*np.pi*ux)
+    # if convention=='+':
+    #     My = xp.exp(1j*2*np.pi*yv) 
+    #     Mx = xp.exp(1j*2*np.pi*ux)
+    # else:
+    #     My = xp.exp(-1j*2*np.pi*yv)
+    #     Mx = xp.exp(-1j*2*np.pi*ux)
 
-    norm_coeff = psf_pixelscale_lamD/npix 
+    # norm_coeff = psf_pixelscale_lamD/npix
+    
+    Mx, My, norm_coeff = make_mft_reverse_matrices(
+        npsf, 
+        psf_pixelscale_lamD, 
+        npix, 
+        N, 
+        convention=convention, 
+        pp_centering=pp_centering, 
+        fp_centering=fp_centering,
+    )
 
     return Mx@fpwf@My * norm_coeff
 
@@ -193,21 +363,52 @@ def get_scaled_coords(N, scale, center=True, shift=True):
     return [cy, cx, r]
 
 def get_fresnel_TF(dz, N, wavelength, fnum):
-    '''
-    Get the Fresnel transfer function for a shift dz from focus
-    '''
+    """
+    Get the Fresnel transfer function for a shift dz from focus. This transfer function
+    is a form of defocus that will be applied to the pupil plane wavefront to compute a 
+    defocused focal plane wavefront. 
+
+    Args:
+        dz (float): 
+            The shift from nominal focus in units of meters. 
+        N (int): 
+            The number of pixels across the array to compute the transfer function for. 
+        wavelength (float):
+            The wavelength to compute the transfer function for in units of meters. 
+        fnum (float): 
+            The F-number of the beam to the focal plane. 
+
+    Returns:
+        ndarray: 
+            The transfer function as a complex phasor. 
+    """
+
     df = 1.0 / (N * wavelength * fnum)
     rp = get_scaled_coords(N,df, shift=False)[-1]
     return xp.exp(-1j*np.pi*dz*wavelength*(rp**2))
 
 def make_vortex_phase_mask(
-        npix, charge=6, 
-        grid='odd', 
-        singularity=None, 
-        focal_length=500*u.mm, 
-        pupil_diam=9.5*u.mm, 
-        wavelength=650*u.nm,
+        npix, 
+        charge=6, 
+        grid='odd'
     ):
+    """
+    Compute the phasor for a vortex phase mask. 
+
+    Args:
+        npix (int): 
+            The number of pixels across the phase mask array. 
+        charge (int, optional): 
+            The number of 2pi phase perturbations along a circular path 
+            centered on the vortex phase mask. Defaults to 6.
+        grid (str, optional): 
+            The centering of the phase mask array. Odd means cenetered on a single pixel, 
+            even means cenetered on the quadrant between four pixels. Defaults to 'odd'.
+
+    Returns:
+        ndarray: 
+            Complex array representing the phasor of the vortex phase mask. 
+    """
     
     if grid=='odd':
         x = xp.linspace(-npix//2, npix//2-1, npix)
@@ -218,11 +419,7 @@ def make_vortex_phase_mask(
 
     phasor = xp.exp(1j*charge*th)
     
-    if singularity is not None:
-        r = xp.sqrt((x-1/2)**2 + (y-1/2)**2)
-        mask = r>(singularity*pupil_diam/(focal_length*wavelength)).decompose().value
-        phasor *= mask
-    
     return phasor
+
 
 
